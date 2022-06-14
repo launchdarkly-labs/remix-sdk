fcd21e286f5a2d2e88a3dc4630f35e0b1320eb2c
Yusinto Ngadiman <yusinto@gmail.com>
2022-06-14 00:13:26 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export const initBrowser = (clientSideId: string) => {
   const options = {
     bootstrap: document.getElementById('ssr-flags') ?? {},
   };
+  console.log('initializing ld client...');
   window.ldClientBrowser = initialize(clientSideId, { anonymous: true }, options);
 };
