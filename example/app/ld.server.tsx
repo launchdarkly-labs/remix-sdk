762b377d151faf2b02503642f9d9980e47a60433
Yusinto Ngadiman <yusinto@gmail.com>
2022-06-15 16:41:21 -0700

    Update ld.server.tsx

--- example/app/ld.server.tsx
+++ example/app/ld.server.tsx
@@ -1,12 +1,8 @@
-import { initLDServerClient, renderFlagsToString } from 'remix-sdk'
-import { v4 } from 'uuid'
+import { initLDServerClient, renderFlagsToString } from 'remix-sdk';
+import { v4 } from 'uuid';
 let client = undefined;
 
 const createClient = async (): Promise<string> => {
-    client = await initLDServerClient(process.env.LD_KEY as string);
-    return v4()
-}
-
-export { createClient, renderFlagsToString  }
-
-
+  client = await initLDServerClient('sdk-90d32e56-3402-4194-a47a-908234da7d43');
+  return v4();
+};
