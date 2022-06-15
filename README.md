acb9e417f93a786a0340e9307133a93a78f8b0d5
Yusinto Ngadiman <yusinto@gmail.com>
2022-06-14 17:01:42 -0700

    Use modd to automatically build sdk and restart example app.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Development
+
+To build the sdk and then run the example app folow these steps.
+
+First, install [modd](https://github.com/cortesi/modd):
+
+```shell
+brew install modd
+```
+
+Then:
+
+```shell
+modd
+```
+
+Then refresh your browser.
--- link-dev.sh
+++ link-dev.sh
@@ -8,5 +8,4 @@ rm -rf example/node_modules/remix-sdk
 mkdir -p example/node_modules/remix-sdk/node_modules
 mkdir -p example/node_modules/remix-sdk/lib
 cp package.json example/node_modules/remix-sdk/package.json
-cp -r node_modules/ example/node_modules/remix-sdk/node_modules/
 cp -r lib/ example/node_modules/remix-sdk/lib/
