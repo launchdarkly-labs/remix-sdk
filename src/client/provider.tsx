bd836f4c16ef511ab4e38038287054fbeb0c3fc7
Brian Lagman <blagman@launchdarkly.com>
2022-06-16 15:05:34 -0700

    Add provider changes

--- src/client/provider.tsx
+++ src/client/provider.tsx
@@ -1,8 +1,8 @@
 import React, { Component, PropsWithChildren } from 'react';
-import { initialize, LDOptions } from 'launchdarkly-js-client-sdk';
-import { ProviderConfig } from './types';
+import { initialize, LDOptions, LDClient, LDFlagChangeset, LDFlagSet } from 'launchdarkly-js-client-sdk';
+import { ProviderConfig, defaultReactOptions } from './types';
 import { Provider, LDContext as HocState } from './context';
-import { camelCaseKeys } from './utils';
+import { camelCaseKeys, getFlattenedFlagsFromChangeset } from './utils';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 class LDProvider extends Component<PropsWithChildren<ProviderConfig>, HocState> {
@@ -13,29 +13,38 @@ class LDProvider extends Component<PropsWithChildren<ProviderConfig>, HocState> 
     const { clientSideID } = props;
 
     console.log(`initializing ld client with ${clientSideID}...`);
-    let ldClient;
-
-    if (typeof window !== 'undefined') {
-      const options = {
-        bootstrap: window.ssrFlags,
-      };
-      ldClient = initialize(clientSideID, { anonymous: true }, options);
-      this.state = {
-        flags: camelCaseKeys(options.bootstrap),
-        ldClient,
-      };
-    } else {
-      console.error(`Fix LDProvider to work on the server`);
-      //TODO: ldClient = this.importInitServerSdk(props.sdkKey, options);
-    }
+    // const ldClient;
+    
+    // if (typeof window !== 'undefined') {
+    const options = {
+      bootstrap: window.ssrFlags,
+    };
+    const ldClient = initialize(clientSideID, { anonymous: true }, options);
+    this.state = {
+      flags: camelCaseKeys(options.bootstrap),
+      ldClient,
+    };
   }
 
-  async importInitServerSdk(clientSideID: string, options: LDOptions) {
-    const { initServerSdk } = await import('../server');
-    return initServerSdk(clientSideID, options);
+  componentDidMount() {
+    this.subscribeToChanges()
+  }
+
+
+  subscribeToChanges = () => {
+    const { flags: targetFlags } = this.state;
+    this.state.ldClient.on('change', (changes: LDFlagChangeset) => {
+      console.log('changes', changes)
+      const flattened: LDFlagSet = getFlattenedFlagsFromChangeset(changes, targetFlags);
+      console.log('flattened', flattened)
+      if (Object.keys(flattened).length > 0) {
+        this.setState(({ flags }) => ({ flags: { ...flags, ...flattened } }));
+      }
+    });
   }
 
   render() {
+    console.log('this.state', this.state)
     return <Provider value={this.state}>{this.props.children}</Provider>;
   }
 }
