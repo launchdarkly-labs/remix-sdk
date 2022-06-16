7614e35fb5711e5d1b65dcb9b648989c3d379086
Yusinto Ngadiman <yusinto@gmail.com>
2022-06-15 17:07:26 -0700

    Update entry.client.tsx

--- example/app/entry.client.tsx
+++ example/app/entry.client.tsx
@@ -2,7 +2,6 @@ import { RemixBrowser } from '@remix-run/react';
 import { hydrate } from 'react-dom';
 import { initBrowser } from 'remix-sdk';
 
-
-initBrowser('CLIENT-SIDE-ID');
+initBrowser(window.env.LD_CLIENT_SIDE_ID);
 
 hydrate(<RemixBrowser />, document);
